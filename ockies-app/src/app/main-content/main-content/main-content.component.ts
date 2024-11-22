46bb79c9000751584b51f7012ee6d8691c472aee
kya512 <ya.nkaujtshuakelly@gmail.com>
2024-11-22 17:44:43 +0100

    création des classes utilisateur et messages-element et les components

--- ockies-app/src/app/main-content/main-content/main-content.component.ts
+++ ockies-app/src/app/main-content/main-content/main-content.component.ts
@@ -1,13 +1,14 @@
 import { Component, OnInit} from '@angular/core';
-
+import { CharteGraphiqueComponent } from '../../charte-graphique/charte-graphique/charte-graphique.component';
 @Component({
   selector: 'app-main-content',
-  imports: [ 
+  imports: [
+    CharteGraphiqueComponent
   ],
   templateUrl: './main-content.component.html',
   styleUrl: './main-content.component.css'
 })
-export class MainContentComponent /*implements OnInit*/{
+export class MainContentComponent {
 
   
 }
